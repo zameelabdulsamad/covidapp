dd572d41021ba80ca66962773cf5e61e8ab75060
zameelabdulsamad <zameelabdulsamad@gmail.com>
2021-05-24 12:06:46 +0530

    firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:covidapp/constants.dart';
 import 'package:covidapp/screens/homeScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+void main () async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Covid Point',
@@ -19,6 +26,8 @@ class MyApp extends StatelessWidget {
         textTheme: Theme.of(context).textTheme.apply(displayColor: primaryText),
 
       ),
+
+
       home: HomeScreen(),
     );
   }
--- lib/screens/districtScreen.dart
+++ lib/screens/districtScreen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:table_calendar/table_calendar.dart';
 import '../constants.dart';
-class DistrictScreen extends StatelessWidget {
+
+class DistrictScreen extends StatefulWidget {
   const DistrictScreen({Key key}) : super(key: key);
 
+  @override
+  _DistrictScreenState createState() => _DistrictScreenState();
+}
+
+class _DistrictScreenState extends State<DistrictScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +17,32 @@ class DistrictScreen extends StatelessWidget {
         backgroundColor: bgGrey,
         elevation: 0,
         title: Text("Malappuram"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding:
+                  EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: bgGrey,
+              ),
+              child: Wrap(
+                children: [
+                  TableCalendar(
 
+                    firstDay: DateTime.utc(2020, 01, 01),
+                    lastDay: DateTime.now(),
+
+                    calendarFormat: CalendarFormat.week,
+                    focusedDay: DateTime.now(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
